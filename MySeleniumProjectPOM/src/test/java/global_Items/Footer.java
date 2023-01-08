35729a4a7c55e328de2ff9ca8a3b095a9475aa96
nicobulzan94 <nicoletabulzan94@gmail.com>
2023-01-08 15:01:33 +0200

    Lab18

--- MySeleniumProjectPOM/src/test/java/global_Items/Footer.java
+++ MySeleniumProjectPOM/src/test/java/global_Items/Footer.java
@@ -1,4 +1,16 @@
 package global_Items;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 public class Footer {
+    private WebDriver driver;
+    public Footer(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+
+
+
 }
